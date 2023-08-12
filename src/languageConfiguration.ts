42dbb9c59c5a1f7bdb2d8c6c92a854ec0433ed14
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-08-12 15:44:12 +0700

    Implement override via setTimeout

--- src/languageConfiguration.ts
+++ src/languageConfiguration.ts
@@ -1,13 +1,13 @@
-import { IndentAction, LanguageConfiguration } from "vscode"
+import { IndentAction, LanguageConfiguration } from 'vscode'
 
 export const languageConfiguration: LanguageConfiguration = {
-    "onEnterRules": [
-        {
-            "beforeText": new RegExp("\"$"),
-            "action": {
-                "indentAction": IndentAction.None
-            }
-        }
-    ],
-    "wordPattern": new RegExp("([^`~@$%^&*()\\-=+\\[{\\]}⟨⟩⦃⦄⟦⟧⟮⟯‹›\\\\|;:\",/\\s]+)", "g")
+  'onEnterRules': [
+    {
+      'beforeText': new RegExp('"$'),
+      'action': {
+        'indentAction': IndentAction.None,
+      },
+    },
+  ],
+  'wordPattern': new RegExp('([^`~@$%^&*()\\-=+\\[{\\]}⟨⟩⦃⦄⟦⟧⟮⟯‹›\\\\|;:",/\\s]+)', 'g'),
 }
