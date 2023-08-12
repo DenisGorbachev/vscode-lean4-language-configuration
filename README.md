c919ef703d8178177652f7ac8232b519576b374e
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2023-08-12 15:43:44 +0700

    Implement override via setTimeout

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 This extension overrides the following language configuration properties:
 
-* `autoClosingPairs` - adds backticks (`\``)
-* `surroundingPairs` - adds backticks (`\``)
+* `autoClosingPairs` - adds backticks (`)
+* `surroundingPairs` - adds backticks (`)
 * `onEnterRules` - adds a rule to skip indenting the new line after pressing `Enter` next to a quote (`"`)
+* `wordPattern` - removes the dot (.), so that fully qualified Lean names become "words"
+  * Example: `Mathlib.Data.List.Sort` becomes a "word"
+
+**Gotchas:**
+
+* This extension applies some parts of its language configuration after a 250ms timeout. This is a hack to ensure that it overrides the main extension (`lean4`). This hack is required because there's no way to specify the extension load order to ensure that this extension always overrides the main extension.
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,22 @@
 // Import the module and reference it with the alias vscode in your code below
 // import * as vscode from 'vscode';
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext, languages } from 'vscode'
+import { languageConfiguration } from './languageConfiguration'
 
-
-export function activate(context: ExtensionContext) { }
+export function activate(context: ExtensionContext) {
+  /**
+   * IMPORTANT: There are two language configurations
+   * - language-configuration.json holds the less important configuration
+   * - src/languageConfiguration.ts holds the more important configuration
+   * The reason is that the more important configuration must be loaded after the main `lean4` extension (to override it)
+   * Hence setTimeout(..., 250)
+   * 
+   * There is no way to specify the extension load order in VSCode
+   */
+  setTimeout(() => {
+    languages.setLanguageConfiguration('lean4', languageConfiguration)
+  }, 250)
+}
 
 // This method is called when your extension is deactivated
 export function deactivate() { }
